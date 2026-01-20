20173384ccd6059b4e1ea4877fed885d06652bd0
Steve Sanderson <SteveSandersonMS@users.noreply.github.com>
2026-01-20 13:08:05 +0000

    Formatting

--- python/e2e/testharness/__init__.py
+++ python/e2e/testharness/__init__.py
@@ -4,4 +4,10 @@ from .context import CLI_PATH, E2ETestContext
 from .helper import get_final_assistant_message, get_next_event_of_type
 from .proxy import CapiProxy
 
-__all__ = ["CLI_PATH", "E2ETestContext", "CapiProxy", "get_final_assistant_message", "get_next_event_of_type"]
+__all__ = [
+    "CLI_PATH",
+    "E2ETestContext",
+    "CapiProxy",
+    "get_final_assistant_message",
+    "get_next_event_of_type",
+]
