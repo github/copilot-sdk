ec854d90371a4e87de06489ba979c4c923dda64c
Steve Sanderson <SteveSandersonMS@users.noreply.github.com>
2026-05-06 15:47:48 -0400

    Formatting

--- rust/src/generated/rpc.rs
+++ rust/src/generated/rpc.rs
@@ -8,8 +8,7 @@
 #![allow(missing_docs)]
 #![allow(clippy::too_many_arguments)]
 
-use super::api_types::rpc_methods;
-use super::api_types::*;
+use super::api_types::{rpc_methods, *};
 use crate::session::Session;
 use crate::{Client, Error};
 
